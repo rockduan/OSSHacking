5918d202ee608db553bebebd45874633d7ae55b0
duanliang <duanrock@foxmail.com>
2020-07-21 20:13:47 +0800

    add info of fingerprinting

---
+++ mechanisms-of-attack/fingerprinting/active-os-fingerprinting/download.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mitmproxy/mitmproxy.git
+git clone https://github.com/electron/fiddle.git
+git clone https://github.com/wireshark/wireshark.git
+git clone https://github.com/fingerprintjs/fingerprintjs2.git
---
+++ mechanisms-of-attack/fingerprinting/active-os-fingerprinting/readme.txt
@@ -0,0 +1,5 @@
+you can learn passive fingerprinting and active finger printing from here:
+english:
+https://www.sciencedirect.com/topics/computer-science/passive-fingerprinting
+chinese:
+https://zhuanlan.zhihu.com/p/37778645
